b488c21a2108426a2fb63671affe3cab238d7960
VKeiby <vop@list.ru>
2020-04-02 19:29:42 +0300

    3 tests

--- test.py
+++ test.py
@@ -1,15 +1,27 @@
 import os,sys
 sys.path.append("../Python_less/")
-# import Lesson5
+import Lesson4
 import pytest
 
 print('this is master branch pyTEST')
 print ('This branch is testing assignment from lesson 6')
 
-from Lesson5 import MassiveP
+from Lesson4 import f,countNsort,topWord,lessLetter
 
-def test_1_CanonicNum():
-    assert CanonicNum(6) == 8
-# def test_get_divisors(divisors_10):
-#     assert dv.get_divisors(10) == divisors_10
-132
+
+def test_f():
+    # Генерация массива определенной длины из вводимого массива
+    nameList = ['Vasya','Fedor','Sasha']
+    assert len(f(nameList,20)) == 20
+
+
+def test_countNsort():
+    # Вычисление повторяющихся элементов
+    integer_list = [34,54,34,54,34,54,34,54,34]
+    assert countNsort(integer_list) == [(34, 5), (54, 4)]
+
+def test_lessLetter():
+    # Выводит первую букву последнего элемента массива
+    sortedDict = [('Vasya',44),('Fedor',33),('Sasha',11)]
+    lessLetter(sortedDict)
+    assert lessLetter(sortedDict) == 'S'
